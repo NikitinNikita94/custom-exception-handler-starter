f661da55615d24b7466016b272405ffca07ad274
nikita <7jay94@mail.ru>
2024-06-03 17:35:40 +0200

    init: init project

---
+++ src/main/java/team/mediasoft/customexceptionhandlerstarter/CustomExceptionHandlerStarterApplication.java
@@ -0,0 +1,13 @@
+package team.mediasoft.customexceptionhandlerstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomExceptionHandlerStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomExceptionHandlerStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/mediasoft/customexceptionhandlerstarter/CustomExceptionHandlerStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package team.mediasoft.customexceptionhandlerstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomExceptionHandlerStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
